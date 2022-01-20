2500564fd8e3a96cac384fb4c20d91323e3adb88
vermei2@outlook.com <vermei2@outlook.com>
2022-01-20 22:08:47 +0300

    Добавьте файлы проекта.

---
+++ lab12/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус");
+            double r = Convert.ToDouble(Console.ReadLine());
+            double length = circle.GetLength(r);
+            double area = circle.GetArea(r);
+           Console.WriteLine($"Длина окружности { length}\n Площадь круга { area}" );
+            Console.WriteLine("Введите координаты центра окружности");
+            double x0 = Convert.ToDouble(Console.ReadLine());
+            double y0 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите координаты точки");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            double point = circle.BelongPoint(x0,y0,x, y);
+            if (point<r)
+                Console.WriteLine("Точка принадлежит кругу");
+            else//иначе
+                Console.WriteLine("Точка не принадлежит кругу");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lab12/circle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab12
+{
+    static class circle
+    {
+        public static double GetLength(double r)
+        {
+            return 2 * Math.PI * r;
+        }
+        public static double GetArea(double r)
+        {
+            return  Math.PI * r*r;
+        }
+         public static double BelongPoint( double x0 , double y0, double x, double y )
+         {
+            return Math.Sqrt((x0 - x) * (x0 - x) + (y0 - y) * (y0 - y));
+         }
+    }
+
+}
